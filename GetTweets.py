9e41786d801ba7b2fc9f77e736c0b976d2ab156c
Ayush Kumar <ayush.kumar@msn.com>
2017-12-03 22:21:49 -0700

    Update GetTweets.py

--- GetTweets.py
+++ GetTweets.py
@@ -27,60 +27,231 @@ status = "Tweeting through Python scripts, Check! #python #twitter #tweepy #data
 api.update_status(status=status)
 
 
-# In[62]:
-
-csvFile = open("Tweets.csv", 'a')
-
-
-# In[63]:
-
-csvWriter = csv.writer(csvFile)
-
-
-# In[66]:
-
 tweets=[]
 
 
-# In[67]:
+# In[39]:
 
-for tweet in tweepy.Cursor(api.sear
-                           /-p;ch,q="#unitedAIRLINES",count=100,
+for tweet in tweepy.Cursor(api.search,q="#iPhoneX",
                            lang="en",
-                           since="2017-04-03").items():
+                           since="2017-12-01").items():
     tweets.append(tweet)
 
 
-# In[68]:
+# In[58]:
 
-tweets_df = pd.DataFrame(vars(tweets[i]) for i in range(len(tweets)))
-
-
-# In[65]:
-
-csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+print("Number of tweets extracted: {}.\n".format(len(tweets)))
+for tweet in tweets[:5]:
+    print(tweet.text)
+    print()
 
 
-# In[69]:
+# In[59]:
 
-FILE_PATH="~/Downloads/Sentimental Analysis/Tweets.csv"
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
 
 
-# In[70]:
+# In[61]:
 
-tweets_df.to_csv(FILE_PATH)
+data.head()
+
+
+# In[74]:
+
+print(tweets[0].id)
+print(tweets[0].created_at)
+print(tweets[0].source)
+print(tweets[0].favorite_count)
+print(tweets[0].retweet_count)
+print(tweets[0].geo)
+print(tweets[0].coordinates)
+print(tweets[0].entities)
+
+
+# In[75]:
+
+data['len']  = np.array([len(tweet.text) for tweet in tweets])
+data['ID']   = np.array([tweet.id for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+
+
+# In[76]:
+
+data.head()
+
+
+# In[5]:
+
+from textblob import TextBlob
+import re
+
+
+# In[78]:
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean the text in a tweet by removing 
+    links and special characters using regex.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+
+# In[100]:
+
+def analyze_sentiment(tweet):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
+
+# In[101]:
+
+data['Sentiment'] = np.array([ analyze_sentiment(tweet) for tweet in data['Tweets'] ])
+
+
+# In[16]:
+
+data.head(10)
+
+
+# In[103]:
+
+FILE_PATH="~/Downloads/Sentimental Analysis/Tweets_Sentiment.csv"
+
+
+# In[105]:
+
+data.to_csv(FILE_PATH)
+
+
+# In[6]:
+
+data = pd.read_csv('Tweets_Sentiment.csv')
+
+
+# In[7]:
+
+data.head()
+
+
+# In[8]:
+
+data = data[['Tweets','Sentiment']]
+
+
+# In[9]:
+
+data.head()
+
+
+# In[113]:
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split # function for splitting data to train and test sets
+
+
+# In[10]:
+
+import nltk
+
+
+# In[11]:
+
+from nltk.corpus import stopwords
+
+
+# In[12]:
+
+from nltk.classify import SklearnClassifier
 
 
 # In[ ]:
 
-## way to retrieve tweets with sentiment
+# how to install wordcloud
 
-public_tweets = api.search('iPhoneX',lang="en", count=200)
-with open("sentiment1.csv",'w') as scorefile:
-    scoreFileWriter = csv.writer(scorefile) 
-    for tweet in public_tweets:
-        print(tweet.text)
-        analysis = TextBlob(tweet.text)
-        print(analysis.sentiment)
-        scoreFileWriter.writerow([tweet.text,analysis.sentiment])
+
+# In[ ]:
+
+# conda install -c conda-forge wordcloud 
+
+
+# In[15]:
+
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+
+
+# In[16]:
+
+data = pd.read_csv('Tweets_Sentiment.csv')
+data = data[['Tweets','Sentiment']]
+
+
+# In[17]:
+
+from sklearn.model_selection import train_test_split # function for splitting data to train and test sets
+
+
+# In[20]:
+
+# Splitting the dataset into train and test set
+train, test = train_test_split(data,test_size = 0.1)
+# Removing neutral sentiments
+train = train[train.Sentiment != -1]
+
+
+# In[21]:
+
+train_pos = train[ train['Sentiment'] == 1]
+train_pos = train_pos['Tweets']
+train_neg = train[ train['Sentiment'] == 0]
+train_neg = train_neg['Tweets']
+
+
+# In[22]:
+
+def wordcloud_draw(data, color = 'black'):
+    words = ' '.join(data)
+    cleaned_word = " ".join([word for word in words.split()
+                            if 'http' not in word
+                                and not word.startswith('@')
+                                and not word.startswith('#')
+                                and word != 'RT'
+                            ])
+    wordcloud = WordCloud(stopwords=STOPWORDS,
+                      background_color=color,
+                      width=2500,
+                      height=2000
+                     ).generate(cleaned_word)
+    plt.figure(1,figsize=(13, 13))
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
+
+
+# In[24]:
+
+print("Positive words")
+wordcloud_draw(train_pos,'white')
+
+
+# In[ ]:
+
+
+
+
+
 
